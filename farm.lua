b5a2c835263f1b1bfd3732e5b2ebeee4555fa8eb
ignirtoq <ignirtoq@gmail.com>
2016-10-08 17:58:50 -0500

    Updated programs to match API changes.

--- farm.lua
+++ farm.lua
@@ -1,13 +1,11 @@
 args = {...}
 -- Check for API --
-local apiLoaded = os.loadAPI("ig")
-if not apiLoaded then
-  shell.run("pastebin get g8QXZrXa ig")
-  apiLoaded = os.loadAPI("ig")
-  assert(apiLoaded,"Error loading API")
+if not ig then
+  shell.run("pastebin","get","g8QXZrXa","ig")
+  assert(os.loadAPI("ig"),"Error loading API")
 end
 if args[1] then
-  ig.farm(unpack(args))
+  igfarm.farm(unpack(args))
 else
-  ig.farm(3)
+  igfarm.farm(3)
 end
