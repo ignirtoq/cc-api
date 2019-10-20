c8180e31b4994310ad9f8f7462a6950d6058f8f4
ignirtoq <ignirtoq@gmail.com>
2019-10-19 18:45:56 -0700

    Fix version string function.  Tweaks to API loading functions.

--- ig.lua
+++ ig.lua
@@ -62,40 +62,38 @@ local function _verFromStr(s)
   if ver[2]:len() < 1 then
     ver[2] = _ver[ver[1]].last
   end
-  if not _ver[ver[2]] then return "Invalid minor version number." end
+  if not _ver[ver[1]][ver[2]] then return "Invalid minor version number." end
   -- Pattern will return empty patch version if only major or only major and  --
   -- minor given. --
   if ver[3]:len() < 1 then
-    ver[3] = _ver[ver[2]].last
+    ver[3] = _ver[ver[1]][ver[2]].last
   end
-  if not _ver[ver[3]] then return "Invalid patch version number." end
+  if not _ver[ver[1]][ver[2]][ver[3]] then
+      return "Invalid patch version number."
+  end
   return "v"..ver[1].."."..ver[2].."."..ver[3]
 end
 
 -- GitHub URLs. --
 local _urlbase = "https://raw.githubusercontent.com/ignirtoq/cc-api/"
-local _urls = {}
-_urls["igrednet"] = "/igrednet.lua"
-_urls["igpower"]  = "/igpower.lua"
-_urls["igturtle"] = "/igturtle.lua"
-_urls["igfarm"]   = "/igfarm.lua"
+
+local function _makeUrl(apiname, version)
+    return _urlbase .. version .. "/" .. apiname .. ".lua"
+end
 
 -- Local paths. --
 local _pathbase = "/ig/"
-local _paths = {}
-_paths["igrednet"] = _pathbase .. "igrednet"
-_paths["igpower"]  = _pathbase .. "igpower"
-_paths["igturtle"] = _pathbase .. "igturtle"
-_paths["igfarm"]   = _pathbase .. "igfarm"
 
-local function _writeApiFile(url, path)
-  assert(type(url)=="string", "First argument must be a valid URL")
-  assert(type(path)=="string", "Second argument must be a valid file path")
-  if fs.exists(path) then fs.delete(path) end
-  local req = http.get(url)
+local function _makePath(apiname)
+    return _pathbase .. apiname
+end
+
+local function _writeApiFile(args)
+  if fs.exists(args.path) then fs.delete(args.path) end
+  local req = http.get(args.url)
   assert(req, "Invalid version.")
-  local f = fs.open(path, "w")
-  f.write(http.get(url).readAll())
+  local f = fs.open(args.path, "w")
+  f.write(http.get(args.url).readAll())
   f.close()
 end
 
@@ -104,8 +102,7 @@ _requiresTurtle["igturtle"] = true
 _requiresTurtle["igfarm"]   = true
 
 -- Check that an API is loaded.  Download and load the API if it is not. --
-function require(apiname,version)
-  assert(_paths[apiname] and _urls[apiname], "Unknown API '"..apiname.."'")
+function require(apiname, version)
   if _requiresTurtle[apiname] then
     assert(turtle, apiname .. " can only be loaded for turtles.")
   end
@@ -114,25 +111,27 @@ function require(apiname,version)
     version = version or "master"
     -- Convert version string to tag/branch name. --
     version = _verFromStr(version)
-    _writeApiFile(_urlbase..version.._urls[apiname], _paths[apiname])
-    assert(os.loadAPI(_paths[apiname]), "Error loading "..apiname.." API.")
+    local path = _makePath(apiname)
+    _writeApiFile{url=_makeUrl(apiname, version), path=path}
+    assert(os.loadAPI(path), "Error loading "..apiname.." API.")
   end
 end
 
 -- Loads the other API components. --
-function loadAPI()
+function loadAPI(args)
+  version = args.version or "master"
   local dirExists = fs.exists("/ig") or fs.makeDir("/ig") or fs.exists("/ig")
   assert(dirExists, "Error creating directory for API")
   -- Get igrednet. --
-  require("igrednet")
+  require("igrednet", version)
   -- Get igpower. --
-  require("igpower")
+  require("igpower", version)
   -- Check if this is a turtle. --
   if turtle then
     -- Get igturtle. --
-    require("igturtle")
+    require("igturtle", version)
     -- Get igfarm. --
-    require("igfarm")
+    require("igfarm", version)
   end
   return true
 end
