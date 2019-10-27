e93fa366101c446b03e98e9105c0de281eb3a12c
ignirtoq <ignirtoq@gmail.com>
2019-10-27 13:58:04 -0700

    Huge restructuring of base API for unit testing.
    The base ig module is now structured to be importable by a standard Lua
    interpreter.  Unit tests for the base module (built with the lua-mock
    public domain module, which lives in a subdirectory of test) live in the
    test directory.
    
    All functions are declared local and only exposed globally as part of the
    public API section.  If loaded in a ComputerCraft context, "global"
    variables are declared with the public function names.  If loaded in the
    standard Lua context, a table with the member variables matching the public
    function names is returned.
    
    Indentation has been uniformly updated to 4 spaces.

--- src/ig.lua
+++ src/ig.lua
@@ -9,24 +9,30 @@
 
 -- Common basic functions. --
 
--- Executes a function once per second until its output is not false or nil.  --
-function waitFor(myfun, args)
-  assert(type(myfun) == "function", "Can only wait for functions.")
-  if args then
-    assert(type(args) == "table",
-           "Second argument must be array of arguments to function.")
-  end
-  args = args or {}
-  local retval = {myfun(unpack(args))}
-  while not retval[1] do
-    os.sleep(1)
-    retval = {myfun(unpack(args))}
-  end
-  return unpack(retval)
+-- Determine if a table is empty.                                             --
+local function _empty(obj)
+    return next(obj) == nil
 end
 
+-- Executes a function once per second until its output is not false or nil.  --
+local function _waitFor(myfun, args)
+    assert(type(myfun) == "function", "Can only wait for functions.")
+    if args then
+        assert(type(args) == "table",
+               "Second argument must be array of arguments to function.")
+    end
+    args = args or {}
+    local retval = {myfun(unpack(args))}
+    while not retval[1] do
+        os.sleep(1)
+        retval = {myfun(unpack(args))}
+    end
+    return unpack(retval)
+end
+
+
 -- Convert an array of items to a set (table mapping items to true).          --
-function arrayToSet(array)
+local function _arrayToSet(array)
     set = {}
     for _, i in ipairs(array) do
         set[i] = true
@@ -34,86 +40,169 @@ function arrayToSet(array)
     return set
 end
 
+
 -- Convert a table of key-value pairs to just an array of values.             --
-function valuesToArray(tbl)
+local function _valuesToArray(tbl)
     local values = {}
     for _, v in pairs(tbl) do
-        values[#values+1] = v
+        table.insert(values, v)
     end
     return values
 end
 
+
 -- Prints out the content of a table when Lua won't. --
-function printTable(tab)
-  assert(type(tab) == "table", "Argument must be a table.")
-  for key, val in pairs(tab) do
-    print(tostring(key)..": "..tostring(val))
-  end
+local function _printTable(tab, indent)
+    assert(type(tab) == "table", "Argument must be a table.")
+    indent = indent or 2
+    -- Convert indent number to space characters. --
+    local sp, i = {}, nil
+    for i = 1,indent,1 do
+        sp[#sp+1] = " "
+    end
+    sp = table.concat(sp, "")
+    -- Print the fields. --
+    if indent == 2 then print("{") end
+    for key, val in pairs(tab) do
+        if type(val) ~= "table" then
+            print(sp..tostring(key)..": "..tostring(val))
+        else
+            print(sp..tostring(key)..": {")
+            _printTable(val, indent+2)
+            print(sp.."}")
+        end
+    end
+    if indent == 2 then print("}") end
 end
 
+
 -- Extend a table's array portion with another table's array portion.         --
-function extendTable(orig, new)
-    for i = 1,#new,1 do
-        orig[#orig+1] = new[i]
-    end
+local function _extendTable(orig, ...)
+    local i
+    local allnew = {...}
+
+    -- Nested repeat-until work-around for lack of "continue" statement.
+    -- See https://stackoverflow.com/a/13825260.
+    for _, new in pairs(allnew) do repeat
+        if type(new) ~= "table" then
+            break  -- a.k.a. "continue"
+        end
+        for i = 1,#new,1 do
+            table.insert(orig, new[i])
+        end
+    until true end
+
+    return orig
 end
+
 
 -------------------------
 -- Inheritance Helpers --
 -------------------------
 -- Set one table to use another table as an attribute lookup.                 --
-function clone(existing, new)
+local function _clone(existing, new)
+    new = new or {}
+    assert(type(existing) == "table", "clone arguments must be tables")
+    assert(type(new) == "table", "clone arguments must be tables")
     new.__index = existing
     return setmetatable(new, new)
 end
 
+
+--------------------------------
+-- Module Loading Abstraction --
+--------------------------------
+-- The ig API is broken up into separate modules.  To help script/program
+-- writers, the whole API is versioned, and this base API provides loading
+-- functions that respect varying levels of version strictness a developer
+-- may require.
+--
+-- Since ComputerCraft and standard Lua provide different base modules,
+-- some normally base Lua functionality is abstracted out so that the API
+-- can be loaded into a ComputerCraft computer/turtle as well as unit-tested
+-- from a standard Lua installation.
+
+
+-- CC/Lua abstractions --
+local function _isCC()
+    return type(os.loadAPI) ~= "nil"
+end
+
+
+local function _loadModule(name, path)
+    if _isCC() then
+        -- ComputerCraft load. --
+        return os.loadAPI(path)
+    else
+        -- Standard Lua load. --
+        _G[name] = require(path)
+        -- Check that the module isn't empty. --
+        return next(_G[name]) ~= nil
+    end
+end
+
+
 -- API versions --
 local _ver = {last = "0"}
 _ver["0"] = {last = "1"}
-_ver["0"]["0"] = {last = "0"}
-_ver["0"]["0"]["0"] = true
 _ver["0"]["1"] = {last = "0"}
 _ver["0"]["1"]["0"] = true
+_ver["0"]["0"] = {last = "0"}
+_ver["0"]["0"]["0"] = true
+
 
 local function _verFromStr(s)
-  assert(type(s) == "string", "Must be a string.")
-  -- If the version is a word (i.e. branch name), return it. --
-  if s:find("%a") then return s end
-  -- Otherwise version must start with a number. --
-  assert(s:find("%d")==1, "Invalid version string.")
-  -- Extract the major, minor, patch version from string. --
-  ver = {s:match("(%d*)%.?(%d*)%.?(%d*)")}
-  assert(ver[1]:len() > 0, "Must supply major version.")
-  if not _ver[ver[1]] then return "Invalid major version number." end
-  -- Pattern will return empty minor version if only major given. --
-  if ver[2]:len() < 1 then
-    ver[2] = _ver[ver[1]].last
-  end
-  if not _ver[ver[1]][ver[2]] then return "Invalid minor version number." end
-  -- Pattern will return empty patch version if only major or only major and  --
-  -- minor given. --
-  if ver[3]:len() < 1 then
-    ver[3] = _ver[ver[1]][ver[2]].last
-  end
-  if not _ver[ver[1]][ver[2]][ver[3]] then
-      return "Invalid patch version number."
-  end
-  return "v"..ver[1].."."..ver[2].."."..ver[3]
+    assert(type(s) == "string", "Must be a string.")
+
+    -- If the version is a word (i.e. branch name), return it. --
+    if s:find("%a") then return s end
+
+    -- Otherwise version must start with a number. --
+    assert(s:find("%d")==1, "Invalid version string.")
+
+    -- Extract the major, minor, patch version from string. --
+    ver = {s:match("(%d*)%.?(%d*)%.?(%d*)")}
+    assert(ver[1]:len() > 0, "Must supply major version.")
+    if not _ver[ver[1]] then
+        assert("invalid major version number")
+    end
+    -- Pattern will return empty minor version if only major given. --
+    if ver[2]:len() < 1 then
+        ver[2] = _ver[ver[1]].last
+    end
+    if not _ver[ver[1]][ver[2]] then
+        assert("invalid minor version number")
+    end
+    -- Pattern will return empty patch version if only major or only major    --
+    -- and minor given. --
+    if ver[3]:len() < 1 then
+        ver[3] = _ver[ver[1]][ver[2]].last
+    end
+    if not _ver[ver[1]][ver[2]][ver[3]] then
+        assert("invalid patch version number")
+    end
+    return "v"..ver[1].."."..ver[2].."."..ver[3]
 end
+
 
 -- First-party URLs --
 local _urlbase = "https://raw.githubusercontent.com/ignirtoq/cc-api/"
+
 
 local function _makeUrl(apiname, version)
     return _urlbase .. version .. "/src/" .. apiname .. ".lua"
 end
 
+
 -- Local paths. --
-local _pathbase = "/ig/"
+local _pathbase = {cc="/ig/", lua="./ig/"}
+
 
 local function _makePath(apiname)
-    return _pathbase .. apiname
+    local base = _isCC() and _pathbase.cc or _pathbase.lua
+    return base .. apiname
 end
+
 
 -- Third-party APIs --
 local _3rdPartyAPIs = {
@@ -121,35 +210,39 @@ local _3rdPartyAPIs = {
     json="https://pastebin.com/raw/4nRg9CHU"
 }
 
+
 local function _writeUrlToFile(args)
-  if fs.exists(args.path) then fs.delete(args.path) end
-  local req = assert(http.get(args.url), "invalid url")
-  local f = fs.open(args.path, "w")
-  f.write(http.get(args.url).readAll())
-  f.close()
+    if fs.exists(args.path) then fs.delete(args.path) end
+    local req = assert(http.get(args.url), "invalid url")
+    local f = fs.open(args.path, "w")
+    f.write(http.get(args.url).readAll())
+    f.close()
 end
+
 
 local _requiresTurtle = {}
 _requiresTurtle["igturtle"] = true
 _requiresTurtle["igfarm"]   = true
 
+
 -- Check that an API is loaded.  Download and load the API if it is not. --
-function require(apiname, version)
-  if _requiresTurtle[apiname] then
-    assert(turtle, apiname .. " can only be loaded for turtles.")
-  end
-  -- Check if the API is loaded. --
-  if not _G[apiname] then
-    version = version or "master"
-    -- Convert version string to tag/branch name. --
-    version = _verFromStr(version)
-    local path = _makePath(apiname)
-    _writeUrlToFile{url=_makeUrl(apiname, version), path=path}
-    assert(os.loadAPI(path), "Error loading "..apiname.." API.")
-  end
+local function _require(apiname, version)
+    if _requiresTurtle[apiname] then
+        assert(turtle, apiname .. " can only be loaded for turtles.")
+    end
+    -- Check if the API is loaded. --
+    if not _G[apiname] then
+        -- Convert version string to tag/branch name. --
+        version = _verFromStr(version or "master")
+        local path = _makePath(apiname)
+        local err = "Error loading "..apiname.." API."
+        _writeUrlToFile{url=_makeUrl(apiname, version), path=path}
+        assert(_loadModule(apiname, path), err)
+    end
 end
 
-function require3rdParty(apiname)
+
+local function _require3rdParty(apiname)
     assert(_3rdPartyAPIs[apiname], "invalid 3rd-party API name")
     if not _G[apiname] then
         local url, path = _3rdPartyAPIs[apiname], _makePath(apiname)
@@ -158,19 +251,51 @@ function require3rdParty(apiname)
     end
 end
 
+
 -- Loads the other API components. --
-function loadAPI(args)
-  version = args.version or "master"
-  local dirExists = fs.exists("/ig") or fs.makeDir("/ig") or fs.exists("/ig")
-  assert(dirExists, "Error creating directory for API")
-  require("iglogging", version)
-  require("iginput", version)
-  require("igrednet", version)
-  require("igpower", version)
-  -- Check if this is a turtle. --
-  if turtle then
-    require("igturtle", version)
-    require("igfarm", version)
-  end
-  return true
+local function _loadAPI(args)
+    version = args.version or "master"
+    local dirExists = fs.exists("/ig") or fs.makeDir("/ig") or fs.exists("/ig")
+    assert(dirExists, "Error creating directory for API")
+    _require("iglogging", version)
+    _require("iginput", version)
+    _require("igrednet", version)
+    _require("igpower", version)
+    -- Check if this is a turtle. --
+    if turtle then
+        _require("igturtle", version)
+        _require("igfarm", version)
+    end
+    return true
+end
+
+
+----------------
+-- Public API --
+----------------
+-- Support both standard Lua and ComputerCraft module loading. --
+if _isCC() then
+    empty = _empty
+    waitFor = _waitFor
+    arrayToSet = _arrayToSet
+    valuesToArray = _valuesToArray
+    printTable = _printTable
+    extendTable = _extendTable
+    clone = _clone
+    require = _require
+    require3rdParty = _require3rdParty
+    loadAPI = _loadAPI
+else
+    return {
+        empty=_empty,
+        waitFor=_waitFor,
+        arrayToSet=_arrayToSet,
+        valuesToArray=_valuesToArray,
+        printTable=_printTable,
+        extendTable=_extendTable,
+        clone=_clone,
+        require=_require,
+        require3rdParty=_require3rdParty,
+        loadAPI=_loadAPI
+    }
 end
---
+++ test/test_ig.lua
@@ -0,0 +1,241 @@
+local ig = require "src.ig"
+local Mock = require "test.mock.Mock"
+local Spy = require "test.mock.Spy"
+local ValueMatcher = require "test.mock.ValueMatcher"
+
+
+local function MockGlobal(name)
+    local orig = _G[name]
+    local m = Mock(orig)
+    m._original = orig
+    function m:clear()
+        _G[name] = self._original
+    end
+    return m
+end
+
+
+local function SpyGlobal(name)
+    local orig = _G[name]
+    local s = Spy(orig)
+    s._original = orig
+    function s:clear()
+        _G[name] = self._original
+    end
+    return s
+end
+
+
+local function assertArraysEqual(a, b)
+    local i
+    assert(#a == #b, string.format(
+        "arrays different lengths: %d != %d", #a, #b
+    ))
+
+    for i = 1,#a,1 do
+        assert(a[i] == b[i], string.format(
+            "arrays have different values at %d: %s != %s",
+            i, tostring(a[i]), tostring(b[i])
+        ))
+    end
+end
+
+
+local function assertTablesEqual(a, b)
+    local i
+    local akeys, bkeys = {}, {}
+    for k, _ in pairs(a) do table.insert(akeys, k) end
+    for k, _ in pairs(b) do table.insert(bkeys, k) end
+    assert(#akeys == #bkeys, string.format(
+        "tables have different sizes: %d != %d", #akeys, #bkeys
+    ))
+    for i = 1,#akeys,1 do
+        assert(a[akeys[i]] == b[akeys[i]], string.format(
+            "tables have different values for %s: %s != %s",
+            tostring(akeys[i]), tostring(a[akeys[i]]),
+            tostring(b[akeys[i]])
+        ))
+    end
+end
+
+
+local function _copyNoIndex(tbl)
+    local new = {}
+    for k, v in pairs(tbl) do repeat
+        if k == '__index' then
+            break  -- continue
+        end
+        new[k] = v
+    until true end
+    return new
+end
+
+
+local function test_empty()
+    local emptyTable = {}
+    local nonemptyTable = {1}
+    next = SpyGlobal('next')
+
+    next:assertCallCount(0)
+    assert(ig.empty(emptyTable))
+    next:assertCallCount(1)
+    assert(not ig.empty(nonemptyTable))
+    next:assertCallCount(2)
+
+    next:clear()
+end
+
+
+local function test_waitFor()
+    local _sleep = os.sleep
+
+    -- Pass through. --
+    local function done() return true end
+
+    os.sleep = Mock(os.sleep)
+    assert(ig.waitFor(done))
+    os.sleep:assertCallCount(0)
+
+    -- Fail once. --
+    local x = 0
+    local function failOnce()
+        x = x+1
+        if x == 2 then
+            return true
+        else
+            return false
+        end
+    end
+
+    os.sleep = Mock(os.sleep)
+    os.sleep:whenCalled{with={ValueMatcher.anyNumber}, thenReturn={}}
+    assert(ig.waitFor(failOnce))
+    os.sleep:assertCallCount(1)
+
+    -- Not a function. --
+    assert(not pcall(ig.waitFor, nil))
+
+    os.sleep = _sleep
+end
+
+
+local function test_arrayToSet()
+    local tbl = {}
+    assertTablesEqual(
+        ig.arrayToSet({'a', 1, true, tbl}),
+        {['a']=true, [1]=true, [true]=true, [tbl]=true}
+    )
+end
+
+
+local function test_valuesToArray()
+    local tbl, str = {}, 'hello'
+    local map = {a=1, [true]=false, [tbl]=str}
+    local mapLen = 3
+
+    local arr = ig.valuesToArray(map)
+    -- Values can come in any order in the array, so the best we can do is
+    -- verify the array length and turn the array to a set and check the
+    -- values.
+    assert(#arr == mapLen, string.format(
+        "unexpected array length: %d != %d", #arr, mapLen
+    ))
+    assertTablesEqual(
+        ig.arrayToSet(arr),
+        {[1]=true, [false]=true, [str]=true}
+    )
+end
+
+
+local function test_extendTable()
+    local tbl = {}
+    local function func() end
+    local arrOnly1 = {1, 'hello', true}
+    local arrOnly2 = {true, tbl, func}
+    local noArr1 = {a=1, b=2}
+    local noArr2 = {c=3, d=4, [0]='hi'}
+    local general = {1, 2, buckle='myShoe'}
+    local actual, expected
+
+    -- Basic case --
+    expected = {1, 'hello', true, true, tbl, func}
+    actual = ig.extendTable({}, arrOnly1, arrOnly2)
+
+    assertArraysEqual(expected, actual)
+
+    -- No array part case --
+    expected = {}
+    actual = ig.extendTable({}, noArr1, noArr2)
+
+    assertTablesEqual(expected, actual)
+
+    -- General case, only copy array part --
+    expected = {1, 2}
+    actual = ig.extendTable({}, general)
+
+    assertArraysEqual(expected, actual)
+    -- Also test as a table to ensure no map part is copied.
+    assertTablesEqual(expected, actual)
+
+    -- One array, one map --
+    expected = {unpack(arrOnly1)}
+    actual = ig.extendTable({}, arrOnly1, noArr1)
+
+    assertTablesEqual(expected, actual)
+
+    -- Not an array --
+    expected = {}
+    actual = ig.extendTable({}, 1)
+
+    assertTablesEqual(expected, actual)
+
+end
+
+
+local function test_clone()
+    local existing, new
+
+    -- Basic case --
+    existing = {a=true}
+    new = {}
+
+    assert(new.a == nil, "expected new.a=nil")
+    ig.clone(existing, new)
+    assert(new.a == true, "expected new.a=true")
+    assert(_copyNoIndex(new).a == nil, "expected copy(new).a=nil")
+
+    assert(new.b == nil, "expected new.b=nil")
+    existing.b = true
+    assert(new.b == true, "expected new.b=true")
+    assert(_copyNoIndex(new).b == nil, "expected copy(new).b=nil")
+    new.b = 1
+    assert(new.b == 1, "expected new.b=1")
+    assert(existing.b == true, "expected existing.b=true")
+    assert(_copyNoIndex(new).b == 1, "expected copy(new).b=1")
+    new.b = nil
+    assert(new.b == true, "expected new.b=true")
+
+    -- Not a table --
+    assert(not pcall(ig.clone, 1))
+    assert(not pcall(ig.clone, {}, 1))
+    assert(not pcall(ig.clone, 1, {}))
+end
+
+
+local function test_require()
+
+    -- No-op for "modules" that exist. --
+    _G['test'] = 1
+    assert(pcall(ig.require, 'test'))
+    _G['test'] = nil
+
+end
+
+
+test_empty()
+test_waitFor()
+test_arrayToSet()
+test_valuesToArray()
+test_extendTable()
+test_clone()
+test_require()
