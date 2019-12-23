8b4e1077342b776ef1289074066b0848966bc36f
ignirtoq <ignirtoq@gmail.com>
2019-12-23 06:51:34 -0800

    Bug fix to make Position/Orientation usable in ComputerCraft.

--- src/iggeo.lua
+++ src/iggeo.lua
@@ -2,36 +2,36 @@
 assert(ig, "iggeo API requires ig API")
 
 -- Position abstraction. --
-local Position = {}
-local _PositionMt = getmetatable(Position) or {}
-setmetatable(Position, _PositionMt)
+local _Position = {}
+local _PositionMt = getmetatable(_Position) or {}
+setmetatable(_Position, _PositionMt)
 
 
-function Position:new()
-    return ig.clone(Position, {x=0, y=0, z=0})
+function _Position:new()
+    return ig.clone(_Position, {x=0, y=0, z=0})
 end
 
-function Position:clone(p)
-    return ig.clone(Position, {x=p.x, y=p.y, z=p.z})
+function _Position:clone(p)
+    return ig.clone(_Position, {x=p.x, y=p.y, z=p.z})
 end
 
-function Position:fromGps(g)
-    return ig.clone(Position, {x=g[1], y=g[2], z=g[3]})
-end
-
-
-function Position:copy()
-    return ig.clone(Position, {x=self.x, y=self.y, z=self.z})
+function _Position:fromGps(g)
+    return ig.clone(_Position, {x=g[1], y=g[2], z=g[3]})
 end
 
 
-function Position:distanceTo(other)
+function _Position:copy()
+    return ig.clone(_Position, {x=self.x, y=self.y, z=self.z})
+end
+
+
+function _Position:distanceTo(other)
     return math.abs(self.x - other.x) + math.abs(self.y - other.y) +
            math.abs(self.z - other.z)
 end
 
 
-function Position:add(other)
+function _Position:add(other)
     self.x = self.x + other.x
     self.y = self.y + other.y
     self.z = self.z + other.z
@@ -39,7 +39,7 @@ function Position:add(other)
 end
 
 
-function Position:sub(other)
+function _Position:sub(other)
     self.x = self.x - other.x
     self.y = self.y - other.y
     self.z = self.z - other.z
@@ -47,24 +47,24 @@ function Position:sub(other)
 end
 
 
-function Position.sum(a, b)
-    return Position:clone(a):add(b)
+function _Position.sum(a, b)
+    return _Position:clone(a):add(b)
 end
 
 
-function Position.difference(a, b)
-    return Position:clone(a):sub(b)
+function _Position.difference(a, b)
+    return _Position:clone(a):sub(b)
 end
 
 
-_PositionMt.__add = Position.sum
-_PositionMt.__sub = Position.difference
+_PositionMt.__add = _Position.sum
+_PositionMt.__sub = _Position.difference
 _PositionMt.__tostring = ig.tableToString
 _PositionMt.__eq = function(a, b) return a.x==b.x and a.y==b.y and a.z==b.z end
 
 
 -- Orientation abstraction. --
-local Orientation = {
+local _Orientation = {
     -- Change in position when moving "forward" in given orientation. --
     FORWARD_POS_CHANGE = {
         [0]={x=0, y=0, z=1},
@@ -80,59 +80,59 @@ local Orientation = {
         {x=1, y=0, z=0}
     }
 }
-local _OrientationMt = getmetatable(Orientation) or {}
-setmetatable(Orientation, _OrientationMt)
+local _OrientationMt = getmetatable(_Orientation) or {}
+setmetatable(_Orientation, _OrientationMt)
 
 
-function Orientation:new()
-    return ig.clone(Orientation, {orient=0})
+function _Orientation:new()
+    return ig.clone(_Orientation, {orient=0})
 end
 
 
-function Orientation:clone(o)
-    return ig.clone(Orientation, {orient=o.orient})
+function _Orientation:clone(o)
+    return ig.clone(_Orientation, {orient=o.orient})
 end
 
 
-function Orientation:copy()
-    return ig.clone(Orientation, {orient=self.orient})
+function _Orientation:copy()
+    return ig.clone(_Orientation, {orient=self.orient})
 end
 
 
-function Orientation:add(other)
+function _Orientation:add(other)
     self.orient = (self.orient + other.orient) % 4
     return self
 end
 
 
-function Orientation:sub(other)
+function _Orientation:sub(other)
     self.orient = (self.orient - other.orient) % 4
     return self
 end
 
 
-function Orientation.sum(a, b)
-    return Orientation:clone(a):add(b)
+function _Orientation.sum(a, b)
+    return _Orientation:clone(a):add(b)
 end
 
 
-function Orientation.difference(a, b)
-    return Orientation:clone(a):sub(b)
+function _Orientation.difference(a, b)
+    return _Orientation:clone(a):sub(b)
 end
 
 
-function Orientation:turnLeft()
+function _Orientation:turnLeft()
     return self:add{orient=1}
 end
 
 
-function Orientation:turnRight()
+function _Orientation:turnRight()
     return self:sub{orient=1}
 end
 
 
-_OrientationMt.__add = Orientation.sum
-_OrientationMt.__sub = Orientation.difference
+_OrientationMt.__add = _Orientation.sum
+_OrientationMt.__sub = _Orientation.difference
 _OrientationMt.__tostring = ig.tableToString
 _OrientationMt.__eq = function(a, b) return a.orient==b.orient end
 
@@ -140,6 +140,9 @@ _OrientationMt.__eq = function(a, b) return a.orient==b.orient end
 ----------------
 -- Public API --
 ----------------
-if not ig.isCC() then
-    return {Position=Position, Orientation=Orientation}
+if ig.isCC() then
+    Position = _Position
+    Orientation = _Orientation
+else
+    return {Position=_Position, Orientation=_Orientation}
 end
