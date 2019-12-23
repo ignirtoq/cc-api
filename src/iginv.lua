371014269a1c4066efbaf3df8a1bf6d8417124b3
ignirtoq <ignirtoq@gmail.com>
2019-12-23 06:56:26 -0800

    Add inventory data representation module.

---
+++ src/iginv.lua
@@ -0,0 +1,176 @@
+-- Dependencies. --
+assert(ig, "iginv API requires ig API")
+
+-- Private helper functions. --
+local function ring(size, start)
+    local end_ = (start - 1) % size
+    local value = end_
+    return function()
+        value = (value + 1) % size
+        if value ~= end_ then
+            return value
+        end
+    end
+end
+
+-- ItemType abstraction. --
+local _ItemType = {}
+local _ItemTypeMt = getmetatable(_ItemType) or {}
+setmetatable(_ItemType, _ItemTypeMt)
+_ItemType._types = {}
+
+
+-- Create a new ItemType instance. --
+-- There should only ever be one instance of each type of item, so this new()
+-- method is different from most others where it takes in the item type's name.
+function _ItemType:new(name)
+    assert(name ~= nil, 'ItemType construction requires name string')
+    itype = _ItemType._types[name]
+    if itype == nil then
+        itype = ig.clone(_ItemType, {
+            name=name,
+            max_stack_size=64
+        })
+        _ItemType._types[name] = itype
+    end
+    return itype
+end
+
+function _ItemType:clone(itype)
+    if type(itype) == 'string' then
+        return _ItemType:new(itype)
+    end
+    return _ItemType:new(itype.name)
+end
+
+
+-- ItemStack abstraction. --
+local _ItemStack = {}
+local _ItemStackMt = getmetatable(_ItemStack) or {}
+setmetatable(_ItemStack, _ItemStackMt)
+
+
+function _ItemStack:new()
+    return ig.clone(_ItemStack, {
+        type=_ItemType:new(''),
+        size=1
+    })
+end
+
+function _ItemStack:clone(i)
+    local itype = type(i.type) == "string" and _ItemType:new(i.type) or i.type
+    return ig.clone(_ItemStack, {
+        type=itype,
+        size=i.size
+    })
+end
+
+function _ItemStack:copy()
+    return _ItemStack:clone(self)
+end
+
+function _ItemStack:setType(name)
+    self.type = _ItemType:new(name)
+    return self
+end
+
+
+-- Inventory abstraction. --
+local _Inventory = {}
+local _InventoryMt = getmetatable(_Inventory) or {}
+setmetatable(_Inventory, _InventoryMt)
+
+
+function _Inventory:new()
+    return ig.clone(_Inventory, {
+        size=27,
+        _itemLookup={}
+    })
+end
+
+function _Inventory:clone(inv)
+    return ig.clone(_Inventory, {
+        size=inv.size,
+        _itemLookup={}
+    })
+end
+
+function _Inventory:_updateItemLookup(index, stack)
+    -- If the slot already had something, update that item type first.
+    if self[index] ~= nil then
+        local oldType = self[index].type.name
+        local oldTypeTable = self._itemLookup[oldType]
+        oldTypeTable[index] = nil
+    end
+
+    -- If the new stack is not nil, update item lookup for new item.
+    if stack ~= nil then
+        local newType = stack.type.name
+        local newTypeTable = self._itemLookup[newType]
+        if newTypeTable == nil then
+            self._itemLookup[newType] = {}
+            newTypeTable = self._itemLookup[newType]
+        end
+        newTypeTable[index] = true
+    end
+end
+
+function _Inventory:setSlot(index, stack)
+    assert(type(index) == "number", "slot index must be a number")
+    assert(index >= 0, "slot index must be non-negative")
+    assert(index < self.size, "slot index must be smaller than inventory size")
+    stack = stack == nil and stack or _ItemStack:clone(stack)
+    -- Update lookup before saving to slot to update based on old slot stack.
+    self:_updateItemLookup(index, stack)
+    self[index] = stack
+    return self
+end
+
+function _Inventory:getSlot(index)
+    assert(type(index) == "number", "slot index must be a number")
+    assert(index >= 0, "slot index must be non-negative")
+    assert(index < self.size, "slot index must be smaller than inventory size")
+    return self[index]
+end
+
+function _Inventory:getSlotsWithItemType(itype)
+    itype = _ItemType:clone(itype)
+    return ig.numericalSetToArray(
+        self._itemLookup[itype.name] or {}
+    )
+end
+
+function _Inventory:getFirstEmptySlot(hint)
+    hint = hint or 0
+    local slot
+    for slot in ring(self.size, hint) do
+        if self[slot] == nil then
+            return slot
+        end
+    end
+    return nil
+end
+
+function _Inventory:getEmptySlots()
+    local slot
+    local emptySlots = {}
+    for slot = 0, self.size-1, 1 do
+        if self[slot] == nil then
+            table.insert(emptySlots, slot)
+        end
+    end
+    return emptySlots
+end
+
+
+if ig.isCC() then
+    ItemType = _ItemType
+    ItemStack = _ItemStack
+    Inventory = _Inventory
+else
+    return {
+        ItemType=_ItemType,
+        ItemStack=_ItemStack,
+        Inventory=_Inventory
+    }
+end
---
+++ test/test_iginv.lua
@@ -0,0 +1,269 @@
+ig = require "src.ig"
+iginv = require "src.iginv"
+local Mock = require "test.mock.Mock"
+local Spy = require "test.mock.Spy"
+local ValueMatcher = require "test.mock.ValueMatcher"
+
+
+local function assertArraysEqual(a, b)
+    local i
+    assert(#a == #b, string.format(
+        "arrays different lengths: %d != %d", #a, #b
+    ))
+
+    for i = 1,#a,1 do
+        assert(a[i] == b[i], string.format(
+            "arrays have different values at %d: %s != %s",
+            i, tostring(a[i]), tostring(b[i])
+        ))
+    end
+end
+
+
+local function test_ItemType_new()
+    local firstType = iginv.ItemType:new("type")
+    local firstTypeCopy = iginv.ItemType:new("type")
+    local secondType = iginv.ItemType:new("other type")
+
+    assert(firstType == firstTypeCopy,
+           "expected two item type instances to be the same object")
+    assert(firstType ~= secondType,
+           "expected two different item types to be different objects")
+end
+
+
+local function test_ItemType_clone()
+    local itype = iginv.ItemType:new("type")
+    local clone = iginv.ItemType:clone(itype)
+
+    assert(itype == clone, "expected clone to be same instance")
+    assert(iginv.ItemType:clone("type") == itype,
+           "expected clone of string to be same instance")
+end
+
+
+local function test_ItemStack_new()
+    local stack = iginv.ItemStack:new()
+
+    assert(stack.type.name == '', string.format(
+        'expected "%s", got "%s"', '', stack.type.name
+    ))
+    assert(stack.size == 1, string.format(
+        'expected %d, got %d', 1, stack.size
+    ))
+end
+
+
+local function test_ItemStack_clone()
+    local itype = 'test_ItemStack_clone'
+    local isize = 3
+    local clone = iginv.ItemStack:clone{type=itype, size=isize}
+
+    assert(clone.type.name == itype, string.format(
+        'expected "%s", got "%s"', 'test', clone.type.name
+    ))
+    assert(clone.size == isize, string.format(
+        'expected %d, got %d', isize, clone.size
+    ))
+
+    clone = iginv.ItemStack:clone({
+        type=iginv.ItemType:new(itype),
+        size=isize
+    })
+
+    assert(clone.type == iginv.ItemType:new(itype), string.format(
+        'expected "%s", got "%s"', itype, clone.type.name
+    ))
+    assert(clone.size == isize, string.format(
+        'expected %d, got %d', isize, clone.size
+    ))
+end
+
+
+local function test_ItemStack_copy()
+    local itype = 'test_ItemStack_copy'
+    local stack = iginv.ItemStack:clone{type=itype, size=3}
+    local copy = stack:copy()
+
+    assert(stack.type.name == copy.type.name, string.format(
+        'expected "%s", got "%s"', stack.type.name, copy.type.name
+    ))
+    assert(stack.type == copy.type, 'expected type fields to be same object')
+    assert(stack.size == copy.size, string.format(
+        'expected %d, got %d', stack.size, copy.size
+    ))
+
+    assert(stack ~= copy, 'expected different objects, got same objects')
+end
+
+
+local function test_ItemStack_setType()
+    local stack = iginv.ItemStack:new():setType('test')
+    local itype = iginv.ItemType:new('test')
+
+    assert(stack.type.name == 'test', string.format(
+        'expected "%s", got "%s"', 'test', stack.type.name
+    ))
+    assert(stack.type == itype,
+           'expected stack type to be same object as itype')
+end
+
+
+local function test_Inventory_new()
+    local inv = iginv.Inventory:new()
+
+    assert(inv.size == 27, string.format('expected %d, got %d', 27, inv.size))
+    assert(#inv == 0, string.format('expected %d, got %d', 0, #inv))
+end
+
+
+local function test_Inventory_setSlot()
+    local slot_index = 7
+    local itype = iginv.ItemType:new 'test_Inventory_setSlot'
+    local stack_size = 4
+    local stack = {type=itype, size=stack_size}
+    local inv = iginv.Inventory:new()
+
+    assert(not pcall(inv.setSlot, inv, nil),
+           'expected assertion error when called with nil index')
+
+    inv:setSlot(slot_index, stack)
+    assert(inv[slot_index].type == stack.type, string.format(
+        'expected "%s", got "%s"', stack.type.name, inv[slot_index].type.name
+    ))
+    assert(inv[slot_index].size == stack_size, string.format(
+        'expected %d, got %d', stack.size, inv[slot_index].size
+    ))
+
+    assert(not pcall(inv.setSlot, inv, -1),
+           'expected assertion error when called with negative index')
+    assert(not pcall(inv.setSlot, inv, 27),
+           'expected assertion error when called with too large index')
+end
+
+
+local function test_Inventory_getSlot()
+    local slot_index = 0
+    local inv = iginv.Inventory:new()
+
+    local slot = inv:getSlot(slot_index)
+    assert(slot == nil, string.format('expected nil, got %s', tostring(slot)))
+
+    local itype = 'test_Inventory_getSlot'
+    local size = 1
+    slot_index = 12
+    inv[slot_index] = iginv.ItemStack:clone{type=itype, size=size}
+    slot = inv:getSlot(slot_index)
+    assert(inv[slot_index] == slot,
+           'expected result and item to be same object')
+    assert(slot.type.name == itype, string.format(
+        'expected "%s", got "%s"', itype, slot.type.name
+    ))
+    assert(slot.size == size, string.format(
+        'expected %d, got %d', size, slot.size
+    ))
+
+    assert(not pcall(inv.getSlot, inv, -1),
+           'expected assertion error when called with negative index')
+    assert(not pcall(inv.getSlot, inv, 27),
+           'expected assertion error when called with too large index')
+end
+
+
+local function test_Inventory_getSlotsWithItemType()
+    local itype = 'test_Inventory_getSlotsWithItemType'
+    local isize = 3
+    local testSlots = {3, 6, 9}
+    local istack = iginv.ItemStack:clone{type=itype, size=isize}
+    local inv = iginv.Inventory:new()
+
+    local slots = inv:getSlotsWithItemType(itype)
+    local expArr = {}
+    assertArraysEqual(expArr, slots)
+
+    inv:setSlot(testSlots[1], istack)
+    slots = inv:getSlotsWithItemType(itype)
+    expArr = {testSlots[1]}
+    assertArraysEqual(expArr, slots)
+
+    inv:setSlot(testSlots[2], istack)
+    slots = inv:getSlotsWithItemType(itype)
+    expArr = {testSlots[1], testSlots[2]}
+    assertArraysEqual(expArr, slots)
+
+    inv:setSlot(testSlots[3], istack)
+    slots = inv:getSlotsWithItemType(itype)
+    expArr = testSlots
+    assertArraysEqual(expArr, slots)
+end
+
+
+local function test_Inventory_getFirstEmptySlot()
+    local size = 3
+    local itype = 'test_Inventory_getFirstEmptySlot'
+    local inv = iginv.Inventory:clone{size=size}
+
+    local slot = inv:getFirstEmptySlot()
+    local expected = 0
+    assert(type(slot) == 'number', string.format(
+        'expected number, got %s', type(slot)
+    ))
+    assert(slot == expected, string.format(
+        'expected %d, got %d', expected, slot
+    ))
+
+    slot = inv:getFirstEmptySlot(1)
+    expected = 1
+    assert(type(slot) == 'number', string.format(
+        'expected number, got %s', type(slot)
+    ))
+    assert(slot == expected, string.format(
+        'expected %d, got %d', expected, slot
+    ))
+
+    inv:setSlot(0, {type=itype, size=1})
+    slot = inv:getFirstEmptySlot()
+    expected = 1
+    assert(type(slot) == 'number', string.format(
+        'expected number, got %s', type(slot)
+    ))
+    assert(slot == expected, string.format(
+        'expected %d, got %d', expected, slot
+    ))
+end
+
+
+local function test_Inventory_getEmptySlots()
+    local invsize = 3
+    local itype = 'test_Inventory_getEmptySlots'
+    local stacksize = 10
+    local inv = iginv.Inventory:clone{size=invsize}
+
+    local emptySlots = inv:getEmptySlots()
+    local expArray = {0, 1, 2}
+    assertArraysEqual(expArray, emptySlots)
+
+    inv:setSlot(0, iginv.ItemStack:clone{type=itype, size=stacksize})
+    emptySlots = inv:getEmptySlots()
+    expArray = {1, 2}
+    assertArraysEqual(expArray, emptySlots)
+
+    inv:setSlot(2, iginv.ItemStack:clone{type=itype, size=stacksize})
+    emptySlots = inv:getEmptySlots()
+    expArray = {1}
+    assertArraysEqual(expArray, emptySlots)
+end
+
+
+test_ItemType_new()
+test_ItemType_clone()
+test_ItemStack_new()
+test_ItemStack_clone()
+test_ItemStack_copy()
+test_ItemStack_setType()
+test_Inventory_new()
+test_Inventory_setSlot()
+test_Inventory_getSlot()
+test_Inventory_getSlotsWithItemType()
+test_Inventory_getFirstEmptySlot()
+test_Inventory_getEmptySlots()
